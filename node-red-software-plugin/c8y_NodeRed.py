47012ae6bffb7d530485966664a023bdde821608
mbay <murat.bayram@softwareag.com>
2023-01-03 09:50:29 +0100

    logging

--- node-red-software-plugin/c8y_NodeRed.py
+++ node-red-software-plugin/c8y_NodeRed.py
@@ -20,18 +20,18 @@ client_id = f'{__name__}-operation-client'
 url = 'http://localhost:1880'
 
 logger.debug(f'Using the following local for mqtt: {url}')
-
-array = sys.argv[1].split(",")
-logger.debug(f'Received the following data: {array}')
-data = base64(array[2]).decode('utf-8')
-flowId = array[3]
-type = array[4]
-
-
-client = mqtt_client.Client(client_id)
-client.connect(broker, port)
+logger.debug(f'Using the following client_id for mqtt: {client_id}')
 
 try:
+    array = sys.argv[1].split(",")
+    logger.debug(f'Received the following data: {array}')
+    
+    data = base64(array[2]).decode('utf-8')
+    flowId = array[3]
+    type = array[4]
+
+    client = mqtt_client.Client(client_id)
+    client.connect(broker, port)
     client.publish('c8y/s/us',f'501,{__name__}')
     nodeRed = Connector(url)
     logger.debug("Checking if node-red is runnning locally")
@@ -54,4 +54,7 @@ try:
         client.publish('c8y/s/us',f'502,{__name__},"Error: Node-Red not running."')
     client.publish('c8y/s/us',f'503,{__name__},')
 except Exception as e:
-    client.publish('c8y/s/us',f'502,{__name__},"Error: {e}"')
+    try:
+        client.publish('c8y/s/us',f'502,{__name__},"Error: {e}"')
+    except Exception as e:
+        logger.debug(f'Aborting due to error: {e}')
